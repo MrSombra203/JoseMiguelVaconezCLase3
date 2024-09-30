50b5b4dbc592dfd4855ec3c07408ce4c8d580f66
MrSombra203 <jose.vasconez.andrade@udla.edu.ec>
2024-09-30 08:51:25 -0500

    Cambio 2

---
+++ JoseMiguelVaconezCLase3/Models/Carrera.cs
@@ -0,0 +1,9 @@
+﻿namespace JoseMiguelVaconezCLase3.Models
+{
+    public class Carrera
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+    }
+}
---
+++ JoseMiguelVaconezCLase3/Models/Class.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+
+namespace JoseMiguelVaconezCLase3.Models
+{
+    public class Class
+    {
+        
+        public string IdBanner { get; set; }
+        public string? Nombre { get; set; }
+        public Carrera Carrera { get; set; }
+
+    }
+}
